b6299c17ab4095e7fecd2ff1c4f727cfbbdc8bf6
Diogo Guerreiro <diogo.l.guerreiro@tecnico.ulisboa.pt>
2024-08-07 23:17:06 +0100

    Initial Commit: Feature: frontend features

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import logo from "/Users/diogoguerreiro/Desktop/E-commerce/frontend/src/Components/Assests/logo.png";
+import cart_icon from "/Users/diogoguerreiro/Desktop/E-commerce/frontend/src/Components/Assests/cart_icon.png";
+
+
+const NavBar = () => {
+    
+const [menu, setMenu] = useState("shop");
+
+
+
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="nav-menu">
+        <li onClick={() => {setMenu("shop")}}><Link style={{ textDecoration: 'none'}} to='/'>Shop</Link>{menu === "shop" ?<hr/>:<></>}</li>
+        <li onClick={() => {setMenu("mens")}}><Link style={{ textDecoration: 'none'}} to ='/mens'>Men</Link>{menu === "mens" ?<hr/>:<></>}</li>
+        <li onClick={() => {setMenu("womens")}}><Link style={{ textDecoration: 'none'}} to ='/womens'>Womens</Link>{menu === "womens" ?<hr/>:<></>}</li>
+        <li onClick={() => {setMenu("kids")}}><Link  style={{ textDecoration: 'none'}} to ='/kids'>Kids</Link>{menu === "kids" ?<hr/>:<></>}</li>
+      </ul>
+      <div className="nav-login-cart">
+        <Link to='/login'><button>Login</button></Link>
+        <Link to='/cart'><img src={cart_icon} alt="" /></Link>
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
