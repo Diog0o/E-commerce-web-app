36824410850be29bb8f7fb2254b9627d5a0e5180
Diogo Guerreiro <diogo.l.guerreiro@tecnico.ulisboa.pt>
2024-08-10 12:43:23 +0100

    Feature: Added other devices display- Navbar.css and Hero.css

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -18,6 +18,7 @@ const Hero = () => {
             <p>for everyone</p>
         </div>
         <div className="hero-latest-btn">
+            <div>Latest Collection</div>
             <img src={arrow_icon} alt="" />
         </div>
     </div>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,21 @@
-import React, {useContext, useState} from "react";
+import React, {useContext, useRef, useState} from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import logo from "/Users/diogoguerreiro/Desktop/E-commerce/frontend/src/Components/Assests/logo.png";
 import cart_icon from "/Users/diogoguerreiro/Desktop/E-commerce/frontend/src/Components/Assests/cart_icon.png";
 import { ShopContext } from "../../Context/ShopContext";
-
+import nav_dropdown from "../Assests/nav_dropdown.png";
 
 const NavBar = () => {
     
 const [menu, setMenu] = useState("shop");
 const {getTotalCartItems} = useContext(ShopContext);
+const menuRef = useRef();
 
+const dropdown_toggle = (e) => {
+  menuRef.current.classList.toggle("nav-menu-visible");
+  e.target.classList.toggle("open");
+}
 
 
   return (
@@ -19,7 +24,8 @@ const {getTotalCartItems} = useContext(ShopContext);
         <img src={logo} alt="" />
         <p>SHOPPER</p>
       </div>
-      <ul className="nav-menu">
+      <img className="nav-dropdown" onClick={dropdown_toggle} src={nav_dropdown} alt="" />
+      <ul ref={menuRef} className="nav-menu">
         <li onClick={() => {setMenu("shop")}}><Link style={{ textDecoration: 'none'}} to='/'>Shop</Link>{menu === "shop" ?<hr/>:<></>}</li>
         <li onClick={() => {setMenu("mens")}}><Link style={{ textDecoration: 'none'}} to ='/mens'>Men</Link>{menu === "mens" ?<hr/>:<></>}</li>
         <li onClick={() => {setMenu("womens")}}><Link style={{ textDecoration: 'none'}} to ='/womens'>Womens</Link>{menu === "womens" ?<hr/>:<></>}</li>
